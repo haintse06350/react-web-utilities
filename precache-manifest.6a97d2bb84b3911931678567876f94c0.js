030768cb4b3dbe632d5721d10a09c008aefcfa48
haintse06350 <haint180298@gmail.com>
2020-02-26 16:13:00 +0700

    Updates

---
+++ precache-manifest.6a97d2bb84b3911931678567876f94c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d768209dcdafccf73e05647f947ec9",
+    "url": "/react-web-utilities/index.html"
+  },
+  {
+    "revision": "3e17022c52f7b819a3ca",
+    "url": "/react-web-utilities/static/js/2.ea1e8261.chunk.js"
+  },
+  {
+    "revision": "a8ffaaafd0878b5f8d70a8de4612632a",
+    "url": "/react-web-utilities/static/js/2.ea1e8261.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe5893f8cf8cb8b60582",
+    "url": "/react-web-utilities/static/js/main.eb4ddfc1.chunk.js"
+  },
+  {
+    "revision": "f4931762fdb2115db394",
+    "url": "/react-web-utilities/static/js/runtime-main.85e3755b.js"
+  },
+  {
+    "revision": "30dbfb7245e2cb951ba2aba71ac8ec55",
+    "url": "/react-web-utilities/static/media/036-Excellence.30dbfb72.svg"
+  },
+  {
+    "revision": "36b75643526e78ff6da80c51d1ba0df1",
+    "url": "/react-web-utilities/static/media/041-value.36b75643.svg"
+  },
+  {
+    "revision": "ec9e2a28f61094fd4ffdc4b22e625728",
+    "url": "/react-web-utilities/static/media/042-trophy.ec9e2a28.svg"
+  },
+  {
+    "revision": "03855bf141cc00b92fad2341e1a79c1c",
+    "url": "/react-web-utilities/static/media/Ic_client.03855bf1.svg"
+  },
+  {
+    "revision": "2cc8930a4cfb8d01dbe9a0bd79e6f597",
+    "url": "/react-web-utilities/static/media/Ic_employee.2cc8930a.svg"
+  },
+  {
+    "revision": "126245c6e7bf7d321531a479dd72cef0",
+    "url": "/react-web-utilities/static/media/Ic_orders.126245c6.svg"
+  },
+  {
+    "revision": "3e5c65ce68d7f61d9f23fd70a4400587",
+    "url": "/react-web-utilities/static/media/dashboard.3e5c65ce.svg"
+  }
+]);
